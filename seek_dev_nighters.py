ab11247d06f05747e78a6d28325eaaaf7218538a
bulbule <anyagoremykina@gmail.com>
2017-03-12 19:50:07 +0100

    change style

--- seek_dev_nighters.py
+++ seek_dev_nighters.py
@@ -1,39 +1,40 @@
 import requests
 from datetime import datetime
-import pytz 
+import pytz
 
 URL_DEVMAN = 'https://devman.org/api/challenges/solution_attempts/'
 TIME_START = '00:00:00'
 TIME_END = '05:00:00'
+
 
 def time_decoder(timestamp, timezone):
     timezone = pytz.timezone(timezone)
     time = datetime.fromtimestamp(timestamp, timezone)
     return time.strftime("%H:%M:%S")
 
+
 def load_attempts():
     pages = 1
-    response = requests.get(URL_DEVMAN, params = {'page': pages})
+    response = requests.get(URL_DEVMAN, params={'page': pages})
     while response.status_code == requests.codes.ok:
         for item in response.json()['records']:
             yield {
                 'username': item['username'],
                 'timestamp': item['timestamp'],
                 'timezone': item['timezone'],
-                }
+            }
         pages += 1
-        response = requests.get(URL_DEVMAN, params = {'page': pages})
+        response = requests.get(URL_DEVMAN, params={'page': pages})
 
 
-            
 def check_time(item):
     item_time = time_decoder(item['timestamp'], item['timezone'])
     if TIME_START < item_time <= TIME_END:
         return True
     else:
         return False
-    
-    
+
+
 def get_midnighters():
     midnighters = set()
     for item in load_attempts():
